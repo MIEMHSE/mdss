69e0d5544a4d353ef87f6b37e928f865934fdb0c
profitware <S.Sobko@profitware.ru>
2016-01-02 23:16:41 +0300

    MinDistanceMethod: Alternatives matrix

--- src/mdss/methods/mindistance.clj
+++ src/mdss/methods/mindistance.clj
@@ -1,21 +1,43 @@
 (ns mdss.methods.mindistance
   (:require [loom.graph :as g]
-            [mdss.core :as c]
-            [clojure.math.combinatorics :as combo])
+            [mdss.core :as c])
 
-  (:use [clojure.core.matrix]))
+  (:use [clojure.core.matrix]
+        [clojure.math.combinatorics]))
 
 (defn- range-index
   [pm choices]
   (map #(.indexOf pm %) choices))
 
-(defn- alternatives-matrix
+(defn- get-inner-permutations
   [pm]
   (let [
-    pm-dim (count pm)
+    vecs (filter vector? pm)
+    vecs-positions (map #(.indexOf pm %) vecs)
+    vecs-perms (map permutations vecs)
+    vecs-produced (apply cartesian-product vecs-perms)]
+    (vec
+      (map
+        (fn [vec-produced]
+          (flatten
+            (map-indexed
+              (fn [current-index current-value]
+                (if (vector? current-value)
+                  (nth
+                    vec-produced
+                    (.indexOf vecs-positions current-index))
+                  current-value))
+              pm)))
+        vecs-produced))))
+
+(defn- alternatives-matrix-for-row
+  [pm]
+  (let [
+    pm-vec (vec pm)
+    pm-dim (count pm-vec)
     pm-range (range
-      (reduce min pm)
-      (inc (reduce max pm)))]
+      (reduce min pm-vec)
+      (inc (reduce max pm-vec)))]
     (matrix
       (partition
         pm-dim
@@ -23,29 +45,17 @@
           x pm-range
           y pm-range]
           (compare
-            (.indexOf pm y)
-            (.indexOf pm x)))))))
+            (.indexOf pm-vec y)
+            (.indexOf pm-vec x)))))))
 
-(defn- get-inner-permutations
-  [pm]
+(defn alternatives-matrix
+  [pmx]
   (let [
-    vecs (filter vector? pm)
-    vecs-counts (map count vecs)
-    vecs-positions (map #(.indexOf pm %) vecs)
-    vecs-perms (map permutations vecs)
-    vecs-produced (apply cartesian-product vecs-perms)]
-    (map
-      (fn [vec-produced]
-        (flatten
-          (map-indexed
-            (fn [current-index current-value]
-              (if (vector? current-value)
-                (nth
-                  vec-produced
-                  (.indexOf vecs-positions current-index))
-                current-value))
-            pm)))
-      vecs-produced)))
+    pms (get-inner-permutations pmx)
+    matrices (map alternatives-matrix-for-row pms)
+    (emap
+      #(/ % (count pms))
+      (apply emap + matrices))))
 
 (deftype MinDistanceMethod []
   c/PDecisionSystem
@@ -56,7 +66,7 @@
     (let [
       step (:step input)
       choices (:choices input)
-      pm (combo/permutations (range 1 (inc (count choices))))
+      pm (permutations (range 1 (inc (count choices))))
       ri (range-index pm choices)]
 
       (case step
