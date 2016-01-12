40919254d15334aa6b5bd2714641e3c04907adbb
bug2bug <S.Sobko@profitware.ru>
2016-01-12 04:26:25 +0300

    Windows Setup: Use AppData directory

--- src/mdss/core.clj
+++ src/mdss/core.clj
@@ -12,19 +12,28 @@
 
   (graph [this input]))
 
+(def mdss-dir
+  (doto
+    (io/file
+      (if (re-find #"win" (-> "os.name" System/getProperty s/lower-case))
+        (System/getenv "APPDATA")
+        (System/getProperty "user.home"))
+      ".mdss")
+    (.mkdir)))
+
+(defn- mdss-file
+  [filename]
+  (str (io/file mdss-dir filename)))
+
 (defn -main
   [& args]
   (let [
     arguments {
       :ip "127.0.0.1"
       :port 8889
-      :project "mdss"}
-    mdss-dir (io/file
-      (if (re-find #"win" (-> "os.name" System/getProperty s/lower-case))
-        (System/getenv "APPDATA")
-        (System/getProperty "user.home"))
-      ".mdss")]
-    (.mkdir mdss-dir)
+      :nrepl-port-file (mdss-file ".nrepl-port")
+      :gorilla-port-file (mdss-file ".gorilla-port")
+      :project "mdss"}]
     (g/run-gorilla-server arguments)
     (b/browse-url
       (str
